1f0ddbf14a4c130bf22ac8d837401b9b237bf00d
Tuananh2810 <trantuananh.28102002@gmail.com>
2024-05-02 15:13:04 +0700

    Bai tap Js 1

---
+++ js.js
@@ -0,0 +1,21 @@
+// 3: Quy doi tien
+
+var so = 2;
+var usdt = 23_500;
+var vnd = so * usdt;
+console.log(
+    " so tien  " + so + " usdt duoc quy doi sang VND la " + vnd + " vnd"
+);
+
+// 4: Tinh dien tich, chu vi hinh chu nhat
+var dai = 5;
+var rong = 3;
+var DT = dai * rong;
+var CV = (dai + rong) * 2;
+console.log(" Dien tich là :::" + " " + DT);
+console.log(" chu Vi là :::" + " " + CV);
+
+//  5; Tinh Tong hai chu so
+var number = 25;
+var sumNumber = Math.floor(number / 10 + (number % 10));
+console.log(" Tong hai chu so :::" + " " + sumNumber);
